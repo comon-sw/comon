7a80b4e16798e556e0e9c76240c0c57202841132
KIMHYEONSU <piae4728@gmail.com>
2024-03-23 22:44:58 +0900

    modify IP

--- JS/login.js
+++ JS/login.js
@@ -1,11 +1,11 @@
 function openFindID () {
   const options = 'width=450, height=500, top=180, left=120'
-  window.open('http:///127.0.0.1:5500/HTML/findID.html','_blank', options)
+  window.open('https://obt.leeskyserver.com/HTML/findID.html','_blank', options)
 }
 // 연결 링크 : 현수집 ip
 
 function openFindPW () {
   const options = 'width=450, height=500, top=180, left=120'
-  window.open('http://127.0.0.1:5500/HTML/findPW.html','_blank', options)
+  window.open('https://obt.leeskyserver.com/HTML/findPW.html','_blank', options)
 }
 // 연결 링크 : 현수집 ip

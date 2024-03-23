9d74fbabf1defed4f8b1769e69083222ede536b8
KIMHYEONSU <piae4728@gmail.com>
2024-03-23 11:56:21 +0900

    modify2

---
+++ JS/findID.js
@@ -0,0 +1,5 @@
+function openFindID_result () {
+  const options = 'width=450, height=500, top=180, left=120'
+  window.open('http://127.0.0.1:5500/HTML/findID_result.html','_blank', options)
+}
+// 연결 링크 : 현수집 ip
--- JS/login.js
+++ JS/login.js
@@ -1,4 +1,4 @@
-function openWindow () {
+function openFindID () {
   const options = 'width=450, height=500, top=180, left=120'
   window.open('http:///127.0.0.1:5500/HTML/findID.html','_blank', options)
 }

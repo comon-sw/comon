1faf60afa7990fbfc7229c2768f8521789027be4
KIMHYEONSU <piae4728@gmail.com>
2024-03-25 23:51:22 +0900

    modify

--- JS/findID.js
+++
@@ -1,5 +0,0 @@
-function openFindID_result () {
-  const options = 'width=450, height=500, top=180, left=120'
-  window.open('https://obt.leeskyserver.com/HTML/findID_result.html','_blank', options)
-}
-// 연결 링크 : 현수집 ip
--- JS/findPW.js
+++
@@ -1,5 +0,0 @@
-function openFindPW_result () {
-  const options = 'width=450, height=500, top=180, left=120'
-  window.open('https://obt.leeskyserver.com/HTML/findPW_result.html','_blank', options)
-}
-// 연결 링크 : 현수집 ip
